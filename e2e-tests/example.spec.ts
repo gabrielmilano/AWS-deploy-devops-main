4d5db48d3c1404fa2c45212093adf74968b35ddc
Gabriel Milano Alves <gmilanoalves@gmail.com>
2025-11-29 21:00:37 -0300

    Garante teste E2E de sucesso

--- e2e-tests/example.spec.ts
+++ e2e-tests/example.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
 test('abre painel admin do Strapi', async ({ page }) => {
-  await page.goto('http://localhost:1337/admin');
+  await page.goto('http://127.0.0.1:1337/admin', { waitUntil: 'networkidle' });
   await expect(page).toHaveURL(/admin/);
 });
